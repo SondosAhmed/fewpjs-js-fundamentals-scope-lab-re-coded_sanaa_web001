f0664b79d1917945800924d3491d127b292b19de
alpha-object-4257 <sondosalshami@gmail.com>
2020-07-19 05:53:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-let animal = ""
+let animal = "";
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+
+  return animal;
 }
 
 function add2(n) {
 
   // Feel free to move things around!
-  const two = 2
-  return n + two
+  const two = 2;
+  return n + two;
 
 }
